8f4f02b69f4043a413a8a723e1445fe56b376169
zinovevmax <144679117+zinovevmax@users.noreply.github.com>
2024-12-14 17:37:36 +0300

    Add files via upload

---
+++ lab_1/oop_laba1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+bool isPerfectSquare(int x) {
+    double sqrt_x = std::sqrt(x);
+    return sqrt_x == static_cast<int>(sqrt_x); // Проверяем, является ли корень целым числом
+}
+
+std::pair<int, int> closestPairToNum(int upperLimit) {
+    for (int m = upperLimit - 1; m > 0; --m) {
+        for (int n = m - 1; n > 0; --n) {
+            if (isPerfectSquare(m + n) && isPerfectSquare(m - n)) {
+                return {m, n};
+            }
+        }
+    }
+    return {-1, -1}; // Возвращаем (-1, -1), если подходящая пара не найдена
+}
+
+int main() {
+    int number;  // объявляем переменную name
+    std::cout << "What is your number\n";
+    std::cin >> number;  // считываем её значение с клавиатуры
+    std::cout << "(" << closestPairToNum(number).first << ", " << closestPairToNum(number).second << ")" << std::endl;    
+    return 0;
+}
